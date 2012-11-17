272600b9e9529bdec2d38a9c33f0e2566f3647e4
slevirus <syrus.levirus@gmail.com>
2012-11-17 18:21:15 +0100

    add utils.py

---
+++ knack/gimp/ascii/utils.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+from gimpfu import *
+
+def generate_log(error_str):
+    '''log on console windows in gimp'''
+    handler = gimp.pdb.gimp_message_get_handler()
+    gimp.pdb.gimp_message_set_handler(2)
+    gimp.pdb.gimp_message(error_str)
+    gimp.pdb.gimp_message_set_handler(handler)
+    return True
