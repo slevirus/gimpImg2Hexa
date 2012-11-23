11547aa6e846fa5a5081161afc1163e87594d8d8
slevirus <syrus.levirus@gmail.com>
2012-11-23 23:55:53 +0100

    update license file

--- knack/gimp/ascii/utils.py
+++ knack/gimp/ascii/utils.py
@@ -3,20 +3,20 @@
 '''
     Simon ANDRÉ ©copyright
     Knack Computer ©copyright
-    This file is part of ascii_gimp.
+    This file is part of knack_ascii_gimp.
 
-    Foobar is free software: you can redistribute it and/or modify
+    knack_ascii_gimp is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    knack_ascii_gimp is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with knack_ascii_gimp.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from gimpfu import *
