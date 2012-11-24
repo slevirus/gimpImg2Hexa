e4e8c545c4a6dab4d9de8ab1817f89c06443ddb6
slevirus <syrus.levirus@gmail.com>
2012-11-24 01:27:21 +0100

    update name of plugins(last)

--- knack/gimp/ascii/ControlGimp.py
+++ knack/gimp/ascii/ControlGimp.py
@@ -3,20 +3,20 @@
 '''
     Simon ANDRÉ ©copyright
     Knack Computer ©copyright
-    This file is part of knack_ascii_gimp.
+    This file is part of knack_gimp_ascii.
 
-    knack_ascii_gimp is free software: you can redistribute it and/or modify
+    knack_gimp_ascii is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    knack_ascii_gimp is distributed in the hope that it will be useful,
+    knack_gimp_ascii is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with knack_ascii_gimp.  If not, see <http://www.gnu.org/licenses/>.
+    along with knack_gimp_ascii.  If not, see <http://www.gnu.org/licenses/>.
 '''
 from gimpfu import *
 import os
